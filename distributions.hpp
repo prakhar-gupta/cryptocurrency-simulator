4e6c6d647f71a18f0ae7fb657987ce4d579ebd3f
Prakhar Gupta <prakharguptaiitd@gmail.com>
2018-02-05 17:16:02 +0530

    Added latency calculation distributions

--- distributions.hpp
+++ distributions.hpp
@@ -1,6 +1,10 @@
 #include <cmath>
 using namespace std;
 
+double iRand(int fMin, int fMax) {
+    return fMin + rand() % (fMax - fMin);
+}
+
 double fRand(double fMin, double fMax) {
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
@@ -9,4 +13,12 @@ double fRand(double fMin, double fMax) {
 double exponential(double lambda) {
     double randNum = fRand(0,1);
     return - log(1 - randNum) / lambda;
+}
+
+double rhoLatency() {
+    fRand(10,500);
+}
+
+double dLatency(double cLatency) {
+    exponential(cLatency / 96);
 }
